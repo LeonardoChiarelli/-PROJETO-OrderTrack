a2cd3c3fbe29bb0b043a3ace77dde5e92f87375d
Leonardo Chiarelli <162373587+LeonardoChiarelli@users.noreply.github.com>
2025-05-27 13:54:12 -0300

    Async Configurations created

---
+++ OrderTrack/Order/src/main/java/br/com/OrderTrack/Order/general/infra/configuration/AsyncConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.OrderTrack.Order.general.infra.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class AsyncConfigurations {
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(50);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+}
