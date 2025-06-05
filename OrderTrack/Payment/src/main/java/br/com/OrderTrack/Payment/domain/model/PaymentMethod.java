ef7db1ae72f43868906142a04eb7eadd835efcc6
Leonardo Chiarelli <162373587+LeonardoChiarelli@users.noreply.github.com>
2025-06-05 16:47:33 -0300

    Changes about Payment Method's case

--- OrderTrack/Payment/src/main/java/br/com/OrderTrack/Payment/domain/model/PaymentMethod.java
+++ OrderTrack/Payment/src/main/java/br/com/OrderTrack/Payment/domain/model/PaymentMethod.java
@@ -1,6 +1,8 @@
 package br.com.OrderTrack.Payment.domain.model;
 
+import br.com.OrderTrack.Payment.domain.dto.RegistryAPaymentMethodDTO;
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,4 +22,13 @@ public class PaymentMethod {
 
     private String name;
     private Boolean active;
+
+    public PaymentMethod(@Valid RegistryAPaymentMethodDTO dto) {
+        this.name = dto.name();
+        this.active = true;
+    }
+
+    public void changeStatus(boolean status) {
+        this.active = status;
+    }
 }
